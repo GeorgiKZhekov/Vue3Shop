78a31fc0afa6d7d258db648848eea9f1eae0e6f1
Zhekov Georgi (HAU-ITDW) <Georgi.Zhekov2@liebherr.com>
2022-09-28 15:13:41 +0300

    init

---
+++ shopping-cart/src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="splash-container">
+    <div class="splash">
+      <h1>Splendid Food</h1>
+    </div>
+  </div>
+  <main class="wrapper">
+    <h2>Recommended</h2>
+    <div class="recommended">
+      <ProductCard 
+        v-for="(product, index) in inventory.slice(0, 3)" 
+        :key="product.id" 
+        :index="index"
+        :product="product"
+        :addToCart="addToCart"
+        class="card"/>
+    </div>
+  </main>
+</template>
+
+<script setup>
+  import ProductCard from '@/components/ProductCardComponent.vue'
+
+  defineProps(['inventory', 'addToCart']);
+</script>
---
+++ shopping-cart/src/views/ProductsView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+    <div class="card-container">
+      <ProductCard 
+        v-for="(product, index) in inventory" 
+        :key="product.id" 
+        :index="index"
+        :product="product"
+        :addToCart="addToCart"
+        class="card"/>
+    </div>
+  </main>
+</template>
+
+<script setup>
+  import ProductCard from '@/components/ProductCardComponent.vue'
+
+  const props = defineProps(['inventory', 'addToCart']);
+</script>
